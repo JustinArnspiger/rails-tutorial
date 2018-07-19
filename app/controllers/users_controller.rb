032949de1cbcf1288b5108d1cfa33132ad97a300
JustinArnspiger <justin.arnspiger@gmail.com>
2018-07-19 10:22:58 -0500

    Create signup page and handling.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Saved successfully
+      flash[:success] = "You have signed up successfully!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
-  get '/signup',   to: 'users#new'
+  get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
   root 'static_pages#home'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
